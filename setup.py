72e3af3f2fd4207f34744cafcf5f7a3a12a33f09
Lorenz Kraus/lkraus-Laptop <lorenz.kraus@gmail.com>
2020-04-11 15:38:45 +0200

    PyPi Setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="degiroapi",
+    version="0.7",
+    author="Lorenz Kraus",
+    author_email="lorenz.kraus@gmail.com",
+    description="An unofficial API for the trading platform Degiro written in Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lolokraus/DegiroAPI",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
